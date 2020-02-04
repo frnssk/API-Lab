fc83d67089408655427683ec6ecfdd988109512f
Jacob Hylén <jacob.hylen@gmail.com>
2020-02-04 11:11:48 +0100

    Added files to test Konva api

--- Jacob/jacob.js
+++ Jacob/jacob.js
@@ -1,5 +1,46 @@
-function jeebaleeba(){
-    console.log("jeebaleeba this is complicated");
-}
+var width = window.innerWidth;
+      var height = window.innerHeight;
 
-jeebaleeba();
+      var stage = new Konva.Stage({
+        container: 'container',
+        width: width,
+        height: height
+      });
+
+      var layer = new Konva.Layer();
+
+      var rect1 = new Konva.Rect({
+        x: 20,
+        y: 20,
+        width: 100,
+        height: 50,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4
+      });
+      // add the shape to the layer
+      layer.add(rect1);
+
+      var rect2 = new Konva.Rect({
+        x: 150,
+        y: 40,
+        width: 100,
+        height: 50,
+        fill: 'red',
+        shadowBlur: 10,
+        cornerRadius: 10
+      });
+      layer.add(rect2);
+
+      var rect3 = new Konva.Rect({
+        x: 50,
+        y: 120,
+        width: 100,
+        height: 100,
+        fill: 'blue',
+        cornerRadius: [0, 10, 20, 30]
+      });
+      layer.add(rect3);
+
+      // add the layer to the stage
+      stage.add(layer);
