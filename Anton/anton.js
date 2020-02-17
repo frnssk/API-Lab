7e851544dd36a812f284db954ca672f53b9efe4a
antonsjoo <60604990+antonsjoo@users.noreply.github.com>
2020-02-17 18:40:36 +0100

    Found examples!

--- Anton/anton.js
+++
@@ -1,8 +0,0 @@
-var osc = new Tone.OmniOscillator();
-osc.frequency.value = "C4";
-osc.start().stop("+8n");
-var env = new Tone.AmplitudeEnvelope();
-osc.connect(env);
-env.toMaster();
-osc.start();
-env.triggerAttack();
