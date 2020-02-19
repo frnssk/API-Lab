6cfddc5a39de3fcab16195d24a3ab45b8857b877
aj8092 <60355017+aj8092@users.noreply.github.com>
2020-02-19 14:30:34 +0100

    Update main.js

--- Viktor/Test 2 RGB colors/main.js
+++ Viktor/Test 2 RGB colors/main.js
@@ -4,12 +4,12 @@ Sketch.create({
     },
     mousemove() {
       this.r = 255 * (this.mouse.x / this.width)
-      this.g = 100 * (this.mouse.y / this.height)
-      this.b = 100 * abs(cos(PI * this.mouse.y / this.width))
+      this.g = 255 * (this.mouse.y / this.height)
+      this.b = 255 * abs(cos(PI * this.mouse.y / this.width))
     
     },
     draw() {
       this.fillStyle = `rgb(${~~this.r},${~~this.g},${~~this.b})`
       this.fillRect(0, 0, this.width, this.height)
     }
-  })
+  });
