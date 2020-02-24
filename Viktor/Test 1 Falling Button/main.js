5d206f8d22f628f687495fe1afd24e1f68d14a1e
aj8092 <60355017+aj8092@users.noreply.github.com>
2020-02-24 13:52:45 +0100

    Changed the button
    Removed the html button and used the clientX and Y parameters to check if the pointer is inside of the falling button. This activates the function for the animation.

--- Viktor/Test 1 Falling Button/main.js
+++ Viktor/Test 1 Falling Button/main.js
@@ -1,80 +1,68 @@
 
-
-
-document.getElementById("myBtn").addEventListener("mousedown", buttonFunction);
-/*document.getElementById("myBtn").addEventListener("mouseup", buttonFunction2);*/
-
-var buttonFallen = false;
-
+/*Creates the "canvas" and draws the button*/
 var ctx = Sketch.create({
   fullscreen: false,
   width: 1000,
   height: 500,
   container: document.getElementById('container'),
-  draw: function() {
+  
+  draw : function() {
 		this.beginPath();
 		this.fillRect(100, 10, 300, 100 );
     this.fillStyle = "#306955";
-    }
-  });
-
-
-
-/* A function that runs the entire animation of the button falling when activated*/ 
-function buttonFunction () {
-  if (buttonFallen === false){
-
-function Vector2(x, y, width, height){ 
-};
-
-
-Vector2.prototype.setPosition = function(x, y) {
-  this.x = x;
-  this.y = y;
-};
-
-/*****FallingBox****/
-
-
-function Box(){
-  this.setPosition(100, 10);
-  this.width = 100;
-  this.height = 35;
-  this.velocityX = 2;
-  this.velocityY = -1;
-  this.color = '#306955';
+    this.strokeText ("Click me to make me drop", 160, 65);
+    this.font = "15px Arial";
   }
+});
 
-Box.prototype = new Vector2;
+document.addEventListener("click", buttonClick);
 
-Box.prototype.update = function() {
-  this.velocityY += 0.4;
-  this.setPosition(this.x + this.velocityX, this.y + this.velocityY);
+/* A function that runs the entire animation of the button falling when the button is clicked*/ 
+function buttonClick(e){
+  if (e.clientX > 110 && e.clientX < 410 && e.clientY > 20 && e.clientY < 120) {
+  function Vector2(x, y, width, height){ 
   };
-
-
-Box.prototype.draw = function() {
-  ctx.fillStyle = this.color;
-  ctx.fillRect(this.x, this.y, this.width, this.height);
+  
+  Vector2.prototype.setPosition = function(x, y) {
+    this.x = x;
+    this.y = y;
   };
-
-ctx.setup = function () {
-  this.player = new Box({x: 100, y: 10, width: 300, height: 100});
-  };
-
-ctx.update = function() {
-  this.player.update();
-  };
-
-ctx.draw = function(){
- 
-  this.player.draw();
+  
+  /*****FallingBox****/
+  
+  /* The falling box's start parameters, size, falling directions and color.*/
+  function Box(){
+    this.setPosition(100, 10);
+    this.width = 300;
+    this.height = 100;
+    this.velocityX = 2;
+    this.velocityY = -1;
+    this.color = '#306955';
+    }
+  
+  Box.prototype = new Vector2;
+  
+  Box.prototype.update = function() {
+    this.velocityY += 0.4;
+    this.setPosition(this.x + this.velocityX, this.y + this.velocityY);
+    };
+  
+  
+  Box.prototype.draw = function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+  
+  ctx.setup = function () {
+    this.player = new Box({x: 100, y: 10, width: 300, height: 100});
+    };
+  
+  ctx.update = function() {
+    this.player.update();
+    };
+  
+  ctx.draw = function(){
+    this.player.draw();
   }; 
-buttonFallen === true;
-
-  } else if (buttonFallen === true)
-  {
-
-  alert("Button has fallen");
   };
 };
