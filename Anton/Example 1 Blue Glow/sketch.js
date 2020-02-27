242e89de7cd17f39dc0f4967452b0145bd21918b
antonsjoo <60604990+antonsjoo@users.noreply.github.com>
2020-02-27 17:05:24 +0100

    added comments to code

--- Anton/Example 1 Blue Glow/sketch.js
+++ Anton/Example 1 Blue Glow/sketch.js
@@ -1,6 +1,15 @@
 var s = Sketch.create({
     autoclear: false
 });
+
+var button = document.getElementById("button")
+
+button.addEventListener("mousedown", function(){
+    hue = minHue = 90;
+    maxHue = 100;
+})
+
+//Setting up, an array for the particles to sit in, and some settings. 
 var particles = [];
 var max = 0;
 var clearColor = "rgba(0,0,0,.2)";
@@ -8,15 +17,24 @@ var hue = minHue = 1;
 var maxHue = 30;
 var reverseHue = false;
 
+//this function is used as an object to hold properties...
 function P() {}
 
+//...such as this one
 P.prototype = {
     constructor: P,
+
+    //method that sets up values for the particles
     init: function() {
+        //generates values that are used when drawing the particles. 
         this.r = random(10);
         this.v = this.r / 8;
+
+        //makes the particles move around the mouse if it is on screen
         this.x = s.mouse.x || s.width / 2 - this.r;
         this.y = s.mouse.y || s.height / 2 - this.r;
+
+        //more generated values
         this.vx = random(-1 * this.v, this.v * 1);
         this.vy = random(-1 * this.v, this.v);
         this.color = "hsla(" + hue + ", 100%, 60%, 10)";
@@ -24,6 +42,8 @@ P.prototype = {
         this.maxLife = random(50);
         this.stroke = Math.random() > .5 ? true : false;
     },
+
+    //method that uses values generated in previous method to draw a particle on the sketch
     draw: function() {
         s.globalCompositeOperation = "source-over";
         s.fillStyle = this.color;
@@ -35,6 +55,8 @@ P.prototype = {
 
         this.update();
     },
+
+    //iterates the particles, updating their values to "kill" old particles
     update: function() {
         this.x += this.vx;
         this.y += this.vy;
@@ -46,6 +68,7 @@ P.prototype = {
     }
 };
 
+//Pushes particles into the array setup early in the code
 s.setup = function() {
     for (var i = 0; i < max; i++) {
         setTimeout(function() {
@@ -55,6 +78,7 @@ s.setup = function() {
         }, i * 10);
     }
 };
+
 
 s.update = function() {
     s.globalCompositeOperation = "source-over";
@@ -72,12 +96,7 @@ s.update = function() {
 
 };
 
-
-
-// s.mousedown = resumeDraw();
-// s.mouseup = stopDraw();
-
-
+//call the method in a for loop to draw all of the particles
 s.draw = function() {
     for (var i in particles) {
         particles[i].draw();
@@ -85,17 +104,17 @@ s.draw = function() {
     }
 };
 
-
+//removes the mouse coords if the mouse leaves the sketch
 s.mouseout = s.touchend = function() {
     s.mouse.x = null;
     s.mouse.y = null;
 };
 
-
+//eventlisteners
 document.addEventListener("mousedown", resumeDraw);
 document.addEventListener("mouseup", stopDraw);
 
-
+//This function starts drawing if you hold down the mouse
 function resumeDraw() {
     max = 20;
 
@@ -109,6 +128,7 @@ function resumeDraw() {
 
 }
 
+//this function stops drawing when you let you of the left click
 function stopDraw() {
     //particles = [];
     for (var i = 0; i < max; i++) {
